7f393b618a0d84e9eb070dec224ab96aa182a10e
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-04 20:44:30 +0300

    Update button.tsx

--- src/button.tsx
+++ src/button.tsx
@@ -4,13 +4,15 @@ type ButtonType = {
     number: number
     clickInc: () => void
     clickReset: () => void
+    maxTitle: number
+    startTitle: number
 }
 
-export const Button: React.FC<ButtonType> = ({number, clickInc, clickReset}) => {
+export const Button: React.FC<ButtonType> = ({number, clickInc, clickReset, maxTitle, startTitle}) => {
     return (
         <div className="button">
-            <button className="buttonInc" disabled={number === 5} onClick={clickInc}>inc</button>
-            <button className="buttonRes" disabled={number === 0} onClick={clickReset}>reset</button>
+            <button className="buttonInc" disabled={number === maxTitle} onClick={clickInc}>inc</button>
+            <button className="buttonRes" disabled={number === startTitle} onClick={clickReset}>reset</button>
         </div>
     );
 };
