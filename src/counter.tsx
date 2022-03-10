ad22702dcf98793610a2489f40f6ea2ab55ca4f4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-10 16:47:01 +0300

    Update counter.tsx

--- src/counter.tsx
+++ src/counter.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "./button";
 
 type CounterType = {
-    number: number
-    clickInc: () => void
-    clickReset: () => void
     maxTitle: number
     startTitle: number
 }
 
-export const Counter: React.FC<CounterType> = ({number, clickReset, clickInc, maxTitle, startTitle}) => {
-
+export const Counter: React.FC<CounterType> = ({maxTitle, startTitle}) => {
+    let [number, setNumber] = useState<number>(0)
+    const clickInc = () => {
+        setNumber(number + 1)
+    }
+    const clickReset = () => {
+        setNumber(startTitle)
+    }
     return (
         <div className={'App'}>
             <div className={number === maxTitle ? 'red' : 'Number'}>{number}</div>
-            <Button number={number} startTitle={startTitle} maxTitle={maxTitle} clickInc={clickInc} clickReset={clickReset}/>
+            <div className={'buttonInc'}>
+            <Button name={'inc'} callBack={clickInc} disable={number === maxTitle}/>
+            </div>
+            <div className={'buttonInc'}>
+            <Button name={'reset'} callBack={clickReset} disable={number === startTitle}/>
+            </div>
         </div>
     );
 };
