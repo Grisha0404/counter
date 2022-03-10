295688f5d0f555bb0bc33a3c9952ba7b11c4fa7a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-10 16:46:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,38 @@ import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from "./counter";
 import {SetCounter} from "./SetCounter";
+import {restoreState, saveState} from "./localStorage";
+import {Button} from "./button";
 
 function App() {
-    let [number, setNumber] = useState<number>(0)
-    let [maxTitle, setMaxTitle] = useState(5)
-    let [startTitle, setStartTitle] = useState(1)
+    let [maxTitle, setMaxTitle] = useState(0)
+    let [startTitle, setStartTitle] = useState(0)
 
-    const clickInc = () => {
-        setNumber(number + 1)
+    const clickSet = () => {
+        saveState('setting', {max: maxTitle, min: startTitle})
     }
-    const clickReset = () => {
-        setNumber(startTitle)
-    }
-    const clickSet = ()=>{
+    useEffect(() => {
+        const state = restoreState('setting', {max: maxTitle, min: startTitle})
+        setMaxTitle(state.max)
+        setStartTitle(state.min)
+    }, [])
 
-    }
     const onChangeMaxTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setMaxTitle(JSON.parse(e.currentTarget.value))
     }
     const onChangeStartTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setStartTitle(JSON.parse(e.currentTarget.value))
     }
-    useEffect(() => {
-        let maxTitleString = localStorage.getItem('counter1')
-        if (maxTitleString) {
-            setMaxTitle(JSON.parse(maxTitleString))
-        }
-    }, [])
-    useEffect(() => {
-        let startTitleString = localStorage.getItem('counter')
-        if (startTitleString) {
-            setStartTitle(JSON.parse(startTitleString))
-        }
-    }, [])
-    useEffect(() => {
-        localStorage.setItem('counter1', JSON.stringify(maxTitle))
-    }, [maxTitle])
-    useEffect(() => {
-        localStorage.setItem('counter', JSON.stringify(startTitle))
-    }, [startTitle])
-
 
     return (
         <div>
-            <Counter number={number} maxTitle={maxTitle} startTitle={startTitle} clickInc={clickInc}
-                     clickReset={clickReset}/>
-            <SetCounter maxTitle={maxTitle} startTitle={startTitle} onChangeMaxTitleHandler={onChangeMaxTitleHandler}
-                        onChangeStartTitleHandler={onChangeStartTitleHandler}/>
-            <button onClick={clickSet}>set</button>
+            <div className={'App'}>
+                <SetCounter maxTitle={maxTitle} startTitle={startTitle}
+                            onChangeMaxTitleHandler={onChangeMaxTitleHandler}
+                            onChangeStartTitleHandler={onChangeStartTitleHandler}/>
+                <Button callBack={clickSet} name={'set'} disable={maxTitle <= startTitle}/>
+            </div>
+                    <Counter maxTitle={maxTitle} startTitle={startTitle}/>
         </div>
     );
 }
